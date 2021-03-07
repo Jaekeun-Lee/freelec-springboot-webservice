9bfc83047240115718af24eb07e29d68b3616ac6
Jaekeun Lee <64590527+Jaekeun-Lee@users.noreply.github.com>
2021-03-07 20:17:16 +0900

    Mustache 템플릿 엔진 화면구성

---
+++ src/main/java/com/jacklee/webservice/web/IndexController.java
@@ -0,0 +1,32 @@
+package com.jacklee.webservice.web;
+
+import com.jacklee.webservice.service.posts.PostsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@RequiredArgsConstructor
+@Controller
+public class IndexController {
+
+    private final PostsService postsService;
+
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("posts", postsService.findAllDesc());
+        return "index";
+    }
+
+    @GetMapping("/posts/save")
+    public String postsSave() {
+        return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+        model.addAttribute("post", postsService.findById(id));
+        return "posts-update";
+    }
+}
--- src/main/java/com/jacklee/webservice/web/PostsApiController.java
+++ src/main/java/com/jacklee/webservice/web/PostsApiController.java
@@ -5,13 +5,10 @@ import com.jacklee.webservice.web.dto.PostSaveRequestDto;
 import com.jacklee.webservice.web.dto.PostsResponseDto;
 import com.jacklee.webservice.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
-@Slf4j
 public class PostsApiController {
 
     private final PostsService postsService;
@@ -28,8 +25,12 @@ public class PostsApiController {
 
     @GetMapping(value = "/api/v1/posts/{id}")
     public PostsResponseDto findById(@PathVariable Long id) {
-        log.debug("id=" + id);
-        log.debug("returnData = " + postsService.findById(id));
         return postsService.findById(id);
+    }
+
+    @DeleteMapping("/api/v1/delete/{id}")
+    public Long postsDelete(@PathVariable Long id) {
+        postsService.delete(id);
+        return id;
     }
 }
