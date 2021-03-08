04bff8bba91742ca32c44239f0686e5090d2cc5f
Jaekeun Lee <64590527+Jaekeun-Lee@users.noreply.github.com>
2021-03-08 19:32:20 +0900

    Spring Security & OAuth 적용

--- src/main/java/com/jacklee/webservice/web/IndexController.java
+++ src/main/java/com/jacklee/webservice/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.jacklee.webservice.web;
 
+import com.jacklee.webservice.config.auth.dto.SessionUser;
 import com.jacklee.webservice.service.posts.PostsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -7,15 +8,23 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
+        if ( user != null ) {
+            model.addAttribute("userName", user);
+        }
         return "index";
     }
 
